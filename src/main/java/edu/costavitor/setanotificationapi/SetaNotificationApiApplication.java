e189bb9fa4773f909a9aaa041cba5b630c317bfb
deltavitor <cvitor1618@gmail.com>
2024-08-17 09:38:10 -0300

    Initial commit

---
+++ src/main/java/edu/costavitor/setanotificationapi/SetaNotificationApiApplication.java
@@ -0,0 +1,13 @@
+package edu.costavitor.setanotificationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetaNotificationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetaNotificationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/costavitor/setanotificationapi/SetaNotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.costavitor.setanotificationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetaNotificationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
