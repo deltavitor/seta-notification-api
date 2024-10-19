2e3f52b12127bfce1b8e9f26462b900ae32440cb
deltavitor <cvitor1618@gmail.com>
2024-10-19 15:18:50 -0300

    Update web client config to allow bigger response sizes

--- src/main/java/edu/costavitor/setanotificationapi/configurations/WebClientConfig.java
+++ src/main/java/edu/costavitor/setanotificationapi/configurations/WebClientConfig.java
@@ -3,6 +3,7 @@ package edu.costavitor.setanotificationapi.configurations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -21,20 +22,32 @@ public class WebClientConfig {
 
     @Bean
     public WebClient geocodingApiWebClient() {
+        final int size = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
 
         DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(
                 UriComponentsBuilder.fromHttpUrl(geocodingApiUrl)
                         .queryParam("key", geocodingApiKey));
 
-        return WebClient.builder().uriBuilderFactory(uriBuilderFactory).build();
+        return WebClient.builder()
+                .exchangeStrategies(strategies)
+                .uriBuilderFactory(uriBuilderFactory).build();
     }
 
     @Bean
     public WebClient ibgeApiWebClient() {
+        final int size = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
 
         DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(
                 UriComponentsBuilder.fromHttpUrl(ibgeApiUrl));
 
-        return WebClient.builder().uriBuilderFactory(uriBuilderFactory).build();
+        return WebClient.builder()
+                .exchangeStrategies(strategies)
+                .uriBuilderFactory(uriBuilderFactory).build();
     }
 }
