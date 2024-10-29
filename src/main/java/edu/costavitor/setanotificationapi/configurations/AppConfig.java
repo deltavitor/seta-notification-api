d2e578539805fbe2f42d3aecf81b045fb4354df7
deltavitor <cvitor1618@gmail.com>
2024-10-29 18:42:47 -0300

    Create LettuceClient configuration for Heroku Redis connection

---
+++ src/main/java/edu/costavitor/setanotificationapi/configurations/AppConfig.java
@@ -0,0 +1,19 @@
+package edu.costavitor.setanotificationapi.configurations;
+
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AppConfig {
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
+    }
+}
+
