06317857611af3e98db25ae6fd88016e27029368
deltavitor <cvitor1618@gmail.com>
2024-10-29 18:22:42 -0300

    Create mechanisms to add notifications from a static file for user testing purposes

--- src/main/java/edu/costavitor/setanotificationapi/common/DBFUtils.java
+++ src/main/java/edu/costavitor/setanotificationapi/common/DBFUtils.java
@@ -1,6 +1,13 @@
 package edu.costavitor.setanotificationapi.common;
 
 import com.linuxense.javadbf.DBFReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class DBFUtils {
 
@@ -9,5 +16,18 @@ public class DBFUtils {
         Integer count = 0;
         while (reader.nextRecord() != null) count++;
         return count;
+    }
+
+    public static MultipartFile getStaticNotificationsDbfAsMultipart() throws IOException {
+        Resource resource = new ClassPathResource("notifications.dbf");
+
+        try (InputStream inputStream = resource.getInputStream()) {
+            return new MockMultipartFile(
+                    "file",
+                    "notifications.dbf",
+                    "multipart/form-data",
+                    inputStream
+            );
+        }
     }
 }
--- src/main/java/edu/costavitor/setanotificationapi/notification_locations/NotificationLocationController.java
+++ src/main/java/edu/costavitor/setanotificationapi/notification_locations/NotificationLocationController.java
@@ -1,5 +1,7 @@
 package edu.costavitor.setanotificationapi.notification_locations;
 
+import edu.costavitor.setanotificationapi.common.DBFUtils;
+import edu.costavitor.setanotificationapi.notifications.NotificationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,8 +18,16 @@ public class NotificationLocationController {
 
     private NotificationLocationService notificationLocationService;
 
+    private NotificationService notificationService;
+
     @GetMapping("/notification-locations")
-    public ResponseEntity<List<NotificationLocation>> findAllNotificationLocations() {
+    public ResponseEntity<List<NotificationLocation>> findAllNotificationLocations() throws IOException {
+
+        // Temporary: use a static file to test the system with end users
+        List<NotificationLocation> notificationLocations = notificationLocationService.findAllNotificationLocations();
+        if (notificationLocations.isEmpty()) {
+            notificationService.addNotificationsFromDbfFile(DBFUtils.getStaticNotificationsDbfAsMultipart());
+        }
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(notificationLocationService.findAllNotificationLocations());
